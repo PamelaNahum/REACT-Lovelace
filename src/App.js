4cd8d43407673e4941eb0245ca5554d6083ee385
PamelaNahum <pamela.nahum14@gmail.com>
2022-12-06 09:15:07 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola mundo caluroso :c</h1>
+      <p>Esto es un parrafo</p>
+      <div>
+        <h2>este es otro div :D</h2>
+      </div>
+      <div>
+        <h3>y este es otro más :D</h3>
+      </div>
     </div>
   );
-}
+};
+const App2 = () => {
+    return (
+      <div>
+        <h1>Hola mundo caluroso :c</h1>
+        <p>Esto es un parrafo</p>
+      </div>
+    );
+  };
 
-export default App;
+export {App, App2};
